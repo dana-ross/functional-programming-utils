d00179160334ceb51c110f3382884590fb9d6fbe
Dave Ross <dave.ross@10up.com>
2015-12-16 16:30:58 -0500

    Fixed 'partial application' semantics. Implemented proper currying.

--- src/curry.php
+++ src/curry.php
@@ -3,7 +3,35 @@
 namespace DaveRoss\FunctionalProgrammingUtils;
 
 /**
- * Curry a function, partially applying the first parameter and returning a function that takes the remaining parameters
+ * Partially apply a function, partially applying the first parameter and returning a function that takes the remaining parameters
+ *
+ * @param callable $x
+ * @param mixed    $y
+ *
+ * @return Closure
+ */
+function partially_apply( callable $x, $y ) {
+	return function () use ( $x, $y ) {
+		return call_user_func_array( $x, array_merge( array( $y ), func_get_args() ) );
+	};
+}
+
+/**
+ * Partially apply a function, partially applying the last parameter and returning a function that takes the remaining parameters
+ *
+ * @param callable $x
+ * @param mixed    $y
+ *
+ * @return Closure
+ */
+function partially_apply_right( callable $x, $y ) {
+	return function () use ( $x, $y ) {
+		return call_user_func_array( $x, array_merge( func_get_args(), array( $y ) ) );
+	};
+}
+
+/**
+ * Curry a function, partially applying a parameter and returning a function that takes the next required parameter until all are satisfied
  *
  * @param callable $x
  * @param mixed    $y
@@ -11,21 +39,21 @@ namespace DaveRoss\FunctionalProgrammingUtils;
  * @return Closure
  */
 function curry( callable $x, $y ) {
-	return function () use ( $x, $y ) {
-		return call_user_func_array( $x, array_merge( array( $y ), func_get_args() ) );
-	};
-}
+	$params = array($y);
+	$required_parameters = (new \ReflectionFunction($x))->getNumberOfRequiredParameters();
 
-/**
- * Curry a function, partially applying the last parameter and returning a function that takes the remaining parameters
- *
- * @param callable $x
- * @param mixed    $y
- *
- * @return Closure
- */
-function curry_right( callable $x, $y ) {
-	return function () use ( $x, $y ) {
-		return call_user_func_array( $x, array_merge( func_get_args(), array( $y ) ) );
-	};
+	if( 1 === $required_parameters ) {
+		return call_user_func_array( $x, array( $y ) );
+	}
+	else {
+		return $fn = function( $z = null ) use ( $x, &$params, $required_parameters, &$fn ) {
+				$params[] = $z;
+				if( count( $params ) === $required_parameters ) {
+					return call_user_func_array( $x, $params );
+				}
+				else {
+					return $fn;
+				}
+		};
+	}
 }
--- tests/php/CurryTest.php
+++ tests/php/CurryTest.php
@@ -1,24 +1,61 @@
 <?php
 
+function three_param($a, $b, $c) {
+		return intval($a) + intval($b) + intval($c);
+}
+
 class CurryTest extends PHPUnit_Framework_TestCase {
 
+	public function test_partially_apply() {
+
+		$fn  = function ( $x, $y ) {
+			return $x + $y;
+		};
+		$cfn = DaveRoss\FunctionalProgrammingUtils\partially_apply( $fn, 2 );
+		$this->assertEquals( 5, $cfn( 3 ) );
+
+	}
+
+	public function test_partially_apply_right() {
+
+		$fn  = function ( $x, $y ) {
+			return $x + $y;
+		};
+		$cfn = DaveRoss\FunctionalProgrammingUtils\partially_apply_right( $fn, 2 );
+		$this->assertEquals( 5, $cfn( 3 ) );
+
+	}
+
 	public function test_curry() {
-
-		$fn  = function ( $x, $y ) {
-			return $x + $y;
-		};
-		$cfn = DaveRoss\FunctionalProgrammingUtils\curry( $fn, 2 );
-		$this->assertEquals( 5, $cfn( 3 ) );
-
+		$pfd = DaveRoss\FunctionalProgrammingUtils\curry( 'sprintf', "%d" );
+		$output = $pfd( 5 );
+		$this->assertEquals( '5', $output );
 	}
 
-	public function test_curry_right() {
-
-		$fn  = function ( $x, $y ) {
-			return $x + $y;
-		};
-		$cfn = DaveRoss\FunctionalProgrammingUtils\curry_right( $fn, 2 );
-		$this->assertEquals( 5, $cfn( 3 ) );
-
+	public function test_curry_single_param_function() {
+		$output = DaveRoss\FunctionalProgrammingUtils\curry( 'intval', 5.2 );
+		$this->assertEquals( 5, $output );
 	}
+
+	public function test_curry_three_param_function() {
+		$fn1_instance1 = DaveRoss\FunctionalProgrammingUtils\curry( 'three_param' , 1 );
+		$fn1_instance2 = $fn1_instance1(2);
+		$output1 = $fn1_instance2(3);
+		$this->assertEquals(6, $output1);
+	}
+
+  /**
+	 * Test that curry() can track parameters for two functions simultaneously
+	 */
+	public function test_curry_multiple_invocations() {
+		$fn1_instance1 = DaveRoss\FunctionalProgrammingUtils\curry( 'three_param' , 1 );
+		$fn2_instance1 = DaveRoss\FunctionalProgrammingUtils\curry( 'three_param' , 10 );
+		$fn1_instance2 = $fn1_instance1(2);
+		$fn2_instance2 = $fn2_instance1(20);
+		$output1 = $fn1_instance2(3);
+		$output2 = $fn2_instance2(30);
+		$this->assertEquals(6, $output1);
+		$this->assertEquals(60, $output2);
+	}
+
 }
